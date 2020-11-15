9893e3277cd89117736bce8ae5c77f439c0ee6f5
James <jqmesnz@gmail.com>
2020-11-15 17:00:42 +1300

    Initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	gifChannel: '777368657029365782',
+	staticChannel: '777368640034701313',
+	delayMilliseconds: 5000,
+	emoji: '<:success:777330653644718130>',
+};
---
+++ gifbot.js
@@ -0,0 +1,46 @@
+'use strict';
+
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const config = require('./config');
+
+const TOKEN = process.env.TOKEN;
+
+const bot = new Discord.Client();
+
+bot.login(TOKEN);
+
+bot.on('ready', () => {
+	console.log(`Logged in as ${bot.user.tag}!`);
+
+	const gifChannel = bot.channels.cache.get(config.gifChannel);
+	const staticChannel = bot.channels.cache.get(config.staticChannel);
+
+	if (!gifChannel || !staticChannel) return console.error('Invalid channel ID.');
+
+	bot.setInterval(() => {
+		sendEmbed(gifChannel, '.gif', true);
+		sendEmbed(staticChannel, '.webp', false);
+	}, config.delayMilliseconds);
+});
+
+function findAvatar(type, dynamic) {
+	const foundUser = bot.users.cache.random();
+	const foundImage = foundUser.avatarURL({ dynamic: dynamic });
+
+	if (foundImage.substr(-type.length) !== type) return findAvatar(type, dynamic);
+
+	else return { user: foundUser, image: foundImage };
+}
+
+function sendEmbed(channel, type, dynamic) {
+	const { user, image } = findAvatar(type, dynamic);
+
+	const embed = new Discord.MessageEmbed()
+		.setDescription(`${config.emoji} Random image arrived! ${config.emoji}`)
+		.setImage(image)
+		.setFooter(`${user.username} | ${user.id}`);
+
+	channel.send(embed).catch(error => console.error(error));
+}
